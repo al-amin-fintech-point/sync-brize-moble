0bc1b5577f801cb43a686dfdf135f97f51783ab1
Al-amin <al-amin@fintech-point.com>
2026-07-30 11:27:41 +0600

    restore project

--- services/socket.js
+++ services/socket.js
@@ -1,6 +1,8 @@
 import { io } from "socket.io-client";
 
-const SERVER_URL = "http://192.168.0.205:3000"; 
+// const SERVER_URL = "http://192.168.0.205:3000"; 
+
+const SERVER_URL = "http://10.96.56.172:3000"; 
 
 export const socket = io( SERVER_URL, {
   transports: [ "websocket" ],
